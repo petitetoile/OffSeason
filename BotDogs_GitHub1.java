9965278c2624f2edad25e133c21121008b6ed857
Daniel Law <drlaw@cps.edu>
2018-11-07 14:39:49 -0600

    Add files via upload

---
+++ BotDogs_GitHub1.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+/**
+ * This class is for testing out GitHub.
+ *
+ * @author Daniel Law
+ * @version 11/7/2018
+ */
+public class BotDogs_GitHub1
+{
+    // instance variables
+    private ArrayList<String> names;
+
+    /**
+     * Constructor for objects of class BotDogs_GitHub1
+     */
+    public BotDogs_GitHub1()
+    {
+        names = new ArrayList<String>();
+    }
+
+    
+    
+    
+    /**
+     * Prints all the strings in names onto separate lines
+     */
+    public void printList()
+    {
+        for ( String n : names )
+        {
+            System.out.println(n);
+        }
+    }
+}
---
+++ BotDogs_GitHub1_Tester.java
@@ -0,0 +1,16 @@
+
+/**
+ * Used to run and test BotDogs_GitHub1
+ *
+ * @author Daniel Law
+ * @version 11/07/2018
+ */
+public class BotDogs_GitHub1_Tester
+{
+    public static void main(String[] args)
+    {
+        BotDogs_GitHub1 obj = new BotDogs_GitHub1();
+        
+        obj.printList();
+    }
+}
